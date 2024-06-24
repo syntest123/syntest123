7ed84ff62d7715ec6360ea46ba0da018bf390054
syntest123 <aaacole6@gmail.com>
2024-06-24 15:57:00 +0200

    Update MainPF.lua

--- MainPF.lua
+++ MainPF.lua
@@ -1450,8 +1450,8 @@ else
 
 	local SettingsTab = Window:CreateTab("Settings");
 	local SettingsInfoSector = SettingsTab:CreateSector("Info", "Left");
-	SettingsInfoSector:CreateCoppyText("Made By m1ckgordon");
-	SettingsInfoSector:CreateCoppyText("https://discord.gg/jNRKdtH3 - Click Me");
+	SettingsInfoSector:CreateCoppyText("Made By Liam");
+	SettingsInfoSector:CreateCoppyText("https://discord.gg/b6aBJbm6fN - Click Me");
 
 	SettingsTab:CreateConfig("Right");
 
